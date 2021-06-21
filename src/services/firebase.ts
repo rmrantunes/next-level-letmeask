da9f375417d38df8a2a67d33bc4e1465fdaefc14
Rafael Antunes <rafaelmessias710@gmail.com>
2021-06-21 09:21:18 -0400

    chore: setup firebase

---
+++ src/services/firebase.ts
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+
+import "firebase/auth";
+import "firebase/databse";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
