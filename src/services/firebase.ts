c7e5b1bd787164ffc04ed9bd3f0df1cb9b637190
Rafael Antunes <rafaelmessias710@gmail.com>
2021-06-29 15:21:40 -0400

    refactor: firebase file

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,16 +1,21 @@
 import firebase from "firebase";
 
-import "firebase/auth";
-import "firebase/databse";
+import "firebase/firebase-auth";
+import "firebase/firebase-database";
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  databaseURL: process.env.DATABASE_URL,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_AxPP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const database = firebase.database();
+
+export { auth, database, firebase };
