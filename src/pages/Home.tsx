19ad0f157a469ea2da50aec89762b541fb8998ff
Rafael Antunes <rafaelmessias710@gmail.com>
2021-06-30 17:42:51 -0400

    feat: add endedAt verify on join room

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -33,6 +33,11 @@ const Home = () => {
       return;
     }
 
+    if (roomRef.val().endedAt) {
+      alert("Room already closed");
+      return;
+    }
+
     history.push(`/rooms/${roomCode}`);
   }
 
