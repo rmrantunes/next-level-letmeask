f87223be0cafb4874d790cb7a482d2b15c26f80e
Rafael Antunes <rafaelmessias710@gmail.com>
2021-06-30 13:39:47 -0400

    feat: add RoomCode

---
+++ src/pages/RoomCode.tsx
@@ -0,0 +1,28 @@
+import copyImg from "assets/copy.svg";
+
+import "styles/room-code.scss";
+
+type RoomCodeProps = {
+  code: string;
+};
+
+const RoomCode = (props: RoomCodeProps) => {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code);
+  }
+
+  return (
+    <button
+      onClick={copyRoomCodeToClipboard}
+      className="room-code"
+      aria-label="Copiar link da sala"
+    >
+      <div>
+        <img src={copyImg} alt="Ícone de copiar" />
+      </div>
+      <span>Sala: {props.code}</span>
+    </button>
+  );
+};
+
+export default RoomCode;
