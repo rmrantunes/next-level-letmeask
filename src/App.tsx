43604ed1c3f26f7b635d156de5f0eb197476554b
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-01 14:24:03 -0400

    refactor: add Routes file

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import "services/firebase";
 
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { AuthContextProvider } from "contexts/AuthContext";
-
-import Home from "pages/Home";
-import NewRoom from "pages/NewRoom";
-import Room from "pages/Room";
-import AdminRoom from "pages/AdminRoom";
+import Routes from "routes/routes";
 
 function App() {
   return (
     <AuthContextProvider>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/rooms/new" component={NewRoom} />
-          <Route path="/rooms/:id" component={Room} />
-          <Route path="/admin/rooms/:id" component={AdminRoom} />
-        </Switch>
-      </BrowserRouter>
+      <Routes />
     </AuthContextProvider>
   );
 }
