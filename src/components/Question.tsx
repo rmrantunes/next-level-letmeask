e0f93f4a59d3666d1853b23541e529e487e8fbbd
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-01 09:56:41 -0400

    feat: highlight & check props & styles in Question

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import cx from "classnames";
+
 import "styles/question.scss";
 
 type QuestionProps = {
@@ -8,11 +10,21 @@ type QuestionProps = {
     avatar: string;
   };
   children?: React.ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
 };
 
 const Question = (props: QuestionProps) => {
   return (
-    <div className="question">
+    <div
+      className={cx(
+        "question",
+        { answered: props.isAnswered },
+        { highlighted: props.isHighlighted && !props.isAnswered }
+      )}
+      {...(props.isHighlighted && { "aria-label": "Pergunta em destaque" })}
+      {...(props.isAnswered && { "aria-label": "Pergunta respondida" })}
+    >
       <p className="content">{props.content}</p>
       <footer>
         <div className="user-info">
