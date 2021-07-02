e687b52115affc295f8f7fd9c050a478dabbcaef
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-02 06:25:51 -0400

    refactor: add SC Button to Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -61,10 +61,14 @@ const Home = () => {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room" onClick={handleCreateRoom}>
+          <Button
+            bgColor="google"
+            className="create-room"
+            onClick={handleCreateRoom}
+          >
             <img src={googleIconImg} alt="Logo do Google" />
             Crie sua sala com o google
-          </button>
+          </Button>
 
           <div className="separator">Ou entre em uma sala</div>
 
