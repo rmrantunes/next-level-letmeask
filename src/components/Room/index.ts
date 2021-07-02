3b6bf5b422541ba876428a5c141ef4874f21bd23
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-02 09:33:35 -0400

    feat: add Room SC styles

---
+++ src/components/Room/index.ts
@@ -0,0 +1,27 @@
+import styled, { css } from "styled-components";
+
+export const FormFooter = styled.div`
+  ${({ theme }) => css`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: ${theme.sizes[4]};
+
+    > span {
+      font-size: ${theme.text.sm};
+      color: ${theme.colors.grayDark};
+      font-weight: ${theme.font.medium};
+
+      button {
+        background: transparent;
+        border: none;
+        color: ${theme.colors.purple};
+        text-decoration: underline;
+        font-size: ${theme.text.sm};
+        font-weight: ${theme.font.medium};
+
+        cursor: pointer;
+      }
+    }
+  `}
+`;
