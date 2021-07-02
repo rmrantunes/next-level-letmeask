512f6cef24f63e67ad46b449df0c54509ec81dd6
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-02 11:37:46 -0400

    feat: implement ThemeModeProvider in index file

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import { ThemeModeProvider } from "contexts/ThemeModeContext";
+
 import "styles/global.scss";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeModeProvider>
+      <App />
+    </ThemeModeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
