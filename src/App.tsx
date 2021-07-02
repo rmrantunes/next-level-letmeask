5084b93a8bef28e6ff986ec9a39f365afd28dc42
Rafael Antunes <rafaelmessias710@gmail.com>
2021-07-02 13:23:12 -0400

    feat: implement useTheme in App

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import "services/firebase";
 import { ThemeProvider } from "styled-components";
 import { AuthContextProvider } from "contexts/AuthContext";
 import Routes from "routes/routes";
-
-import theme from "styles/theme";
+import { useThemeMode } from "hooks/useThemeMode";
 
 function App() {
+  const { theme } = useThemeMode();
+
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={theme!}>
       <AuthContextProvider>
         <Routes />
       </AuthContextProvider>
